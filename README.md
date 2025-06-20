7fbb4f0dea54fcdc011cf8dfb0b56bca39b3ed55
freakymind12 <riankurniawan132@gmail.com>
2025-06-20 08:22:40 +0700

    add node-cron for repetition checking in 20 second, add readme

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# Alert Bot Meeting Room
+
+Whatsapp bot for monitoring tablet connection status is it disconnected or not.
+
+## Getting Started
+
+### Clone Repository
+
+```bash
+git clone https://github.com/freakymind12/bot-wa-booking.git
+cd bot-wa-booking
+```
+
+### Install dependencies
+
+```bash
+npm install
+```
+
+### Setup Environtment Variable
+
+See `.env.example` for template making your environtment variable on file `.env`
+
+### Run with docker-compose
+
+```bash
+docker compose up -d
+```
+
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const qrcode = require("qrcode-terminal")
 const dbPool = require("./knex")
 require('dotenv').config()
 const dayjs = require('dayjs')
+const cron = require('node-cron')
 
 const pic_phone = process.env.PIC_PHONE?.split(',').map(n => n.trim()) || []
 
@@ -20,7 +21,7 @@ async function connectToWhatsApp() {
   socket.ev.on('connection.update', ({ connection, qr }) => {
     if (qr) {
       console.log("Scan QR code below to login:")
-      qrcode.generate(qr, { small: true }) 
+      qrcode.generate(qr, { small: true })
       console.log('\n')// tampilkan QR di terminal
     }
 
@@ -28,7 +29,9 @@ async function connectToWhatsApp() {
       console.log("WA Bot Active")
 
       // Lakukan sesuatu pada whatsapp disini
-      checkingStatusDevice(socket, pic_phone)
+      cron.schedule('*/20 * * * * *', () => {
+        checkingStatusDevice(socket, pic_phone)
+      })
 
     } else if (connection == 'close') {
       console.log("WA Bot Closed")
@@ -53,10 +56,10 @@ async function connectToWhatsApp() {
 
     if (msg.key.fromMe) {
       // Pesan keluar (dikirim oleh bot sendiri)
-      console.log('[MESSAGE OUT]', { remoteJid, conversation })
+      // console.log('[MESSAGE OUT]', { remoteJid, conversation })
     } else {
       // Pesan masuk (dikirim oleh user)
-      console.log('[MESSAGE IN]', { remoteJid, conversation })
+      // console.log('[MESSAGE IN]', { remoteJid, conversation })
     }
   })
 }
@@ -68,9 +71,9 @@ async function sendMessageToPeople(socket, nomorList, isiPesan) {
     const jid = nomor.endsWith('@s.whatsapp.net') ? nomor : nomor + '@s.whatsapp.net'
     try {
       const sent = await socket.sendMessage(jid, { text: isiPesan })
-      console.log(`✅ Pesan terkirim ke ${nomor}`, sent.key.id)
+      console.log(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] | ✅ Pesan terkirim ke ${nomor}`, sent.key.id)
     } catch (error) {
-      console.error(`❌ Gagal kirim ke ${nomor}:`, error.message)
+      console.error(`[${dayjs().format("YYYY-MM-DD HH:mm:ss")}] | ❌ Gagal kirim ke ${nomor}:`, error.message)
     }
   }
 }
@@ -82,11 +85,10 @@ async function checkingStatusDevice(socket, nomorList) {
     .filter(device => device.status === 0)
     .map((device, index) => `${index + 1}. ${device.room_name}\n   Last Connection:\n   ${dayjs(device.updated_at).format("YYYY-MM-DD HH:mm:ss")}\n   Status : ❌ Disconnected`)
 
-
-  const text = '📋 *Information Tablet Meeting Room Status* 📋\n\n' + disconnectedDevices.join('\n\n') + '\n\n⚠️ Need to check tablet device. ⚠️'
-
-
-  sendMessageToPeople(socket, nomorList, text)
+  if (disconnectedDevices.length > 0) {
+    const text = '📋 *Information Tablet Meeting Room Status* 📋\n\n' + disconnectedDevices.join('\n\n') + '\n\n⚠️ Need to check tablet device. ⚠️'
+    sendMessageToPeople(socket, nomorList, text)
+  }
 }
 
 
